6b44a0689541569e3003f6ffb677cedf7847643b
tmq247 <60788746+tmq247@users.noreply.github.com>
2023-11-22 19:06:53 +0700

    Update test2.py

--- test2.py
+++ test2.py
@@ -150,13 +150,13 @@ def handle_message(message,
                    total_bet_X = sum([user_bets[user_id]['X'] for user_id in user_bets])
                    ):
     chat_id = message.chat.id
+    ten_ncuoc = message.from_user.first_name
 
     # Check if the message is from the group chat
     if chat_id == group_chat_id:
         # Check if the message is a valid bet
         if message.text and message.text.upper() in ['/T ALL', '/X ALL'] or (message.text and message.text.upper()[1] in ['T', 'X'] and message.text[3:].isdigit()):
             user_id = message.from_user.id
-            ten_ncuoc = message.from_user.first_name
             bet_type = message.text.upper()[1]
             if message.text.upper() == '/T ALL' or message.text.upper() == '/X ALL':
                 bet_amount = user_balance.get(user_id, 0)  # Use the entire balance
